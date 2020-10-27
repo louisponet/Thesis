65c27def42168b3925c8d84fff3fa792ffde84f0
Unknown <louisponet@gmail.com>
2020-10-27 09:42:06 +0100

    first draft Cr?

--- scripts/Cr.jl
+++ scripts/Cr.jl
@@ -34,29 +34,26 @@ savefig("/home/ponet/Documents/PhD/Thesis/papers/Cr/Images/Theory_Fit.pdf")
 mp = get_model_params(Cb = 1e6, ξ=0.04)
 envelope1 = x -> x < 2π ? 0.5*sin(x) : ( x < 3π ? 0.5/π*(x-2π) : -0.5/π*(x-3π) + 0.5)
 fitrange = -1.0:0.01:4π
-t1 = fit_signal(envelope, fitrange, params=mp, optim_params=get_optim_params(iterations=500),threaded=true)
+t1 = fit_signal(envelope1, fitrange, params=mp, optim_params=get_optim_params(iterations=500),threaded=true)
 fitrange = -1.0:0.002:4π
-plot(fitrange, t1, mp, envelope)
-savefig(papersdir("Cr/Images/fit1.pdf"))
-mp = get_model_params(Cb = 1e6, ξ=0.04)
-#%%
+# plot(fitrange, t1, mp, envelope)
+# savefig(papersdir("Cr/Images/fit1.pdf"))
+# mp = get_model_params(Cb = 1e6, ξ=0.04)
 envelope2 = x -> x < 2π ? 0.5*CrSDW.gaussian(x, π, π/3) : ( x < 3π ? 0.5/2π*(exp((x-2π)/(π/log(2π+1)))-1) : -0.5/2π*(exp((x-3π)/(π/log(2π+1)))-1) + 0.5)
 fitrange = -1.0:0.01:4π
-t2 = fit_signal(envelope, fitrange, params=mp, optim_params=get_optim_params(iterations=500), threaded=true)
+t2 = fit_signal(envelope2, fitrange, params=mp, optim_params=get_optim_params(iterations=500), threaded=true)
 fitrange = -1.0:0.002:4π
-plot(fitrange, t2, mp, envelope)
-savefig(papersdir("Cr/Images/fits.pdf"))
+# plot(fitrange, t2, mp, envelope)
 
 plot(plot(fitrange, t1, mp, envelope1), plot(fitrange, t2, mp, envelope2, legend=nothing), ylims=[-1.5,2.5])
+savefig(papersdir("Cr/Images/fits.pdf"))
 
 #%%
 ## For maximum amplitude
-envelope = x -> 4.0
+envelope = x -> 4.5
 fitrange = -1.0:0.01:4π
 t1 = fit_signal(envelope, fitrange, params=mp, optim_params=get_optim_params(iterations=500),threaded=true)
-fitrange = -1.0:0.002:4π
-plot(fitrange, t1, mp, envelope)
-t2 = fit_signal(envelope, fitrange, params=mp, optim_params=get_optim_params(iterations=500),threaded=true)
+t2 = fit_signal(envelope, fitrange, params=mp, optim_params=get_optim_params(iterations=500),threaded=true, max_amplitude=true)
 fitrange = -1.0:0.002:4π
 plot(plot(fitrange, t1, mp, envelope), plot(fitrange, t2, mp, envelope, legend=nothing))
 savefig(papersdir("Cr/Images/maximumamplitude.pdf"))
