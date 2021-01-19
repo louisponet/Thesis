f1e85e14489ce25b2e9e9d3024b0114ec2513f5a
Unknown <louisponet@gmail.com>
2021-01-19 12:50:07 +0100

    added a way to calculate peierls nabarro barriers

--- scripts/Softening.jl
+++ scripts/Softening.jl
@@ -37,3 +37,25 @@ using BenchmarkTools
 @btime BTO.element_potential($(rand(24)), $(m[1].threadcaches[1].cellvalues), $(m[1].threadcaches[1].extradata), $(BTO.Ffunc(params)), 1.0) 
 
 simulation(params, simparams)
+
+
+### Peierls Nabarro barriers
+mp = ModelParams(xsize = 10e-9, ysize=1e-9, zsize=5e-9, elementsize = 3e-10, F11 =0.0, F12 = 0.0, F44 =0.0)
+sim = SimParams(tipradius = 1e-10, forceconstant = 2.0e29,  gtol=1e-5, centers = [0.0, 1.0e-9, 2.0e-9, 5.0e-9],minimizer=BTO.ConjugateGradient, workdir=datadir("test2"), timelimit=1000000000000, maxsteps=1000)
+m, dofs  = simmodel(mp, sim);
+res = optimize(m)
+m.dofs.=res.minimize
+tree = BTO.Landau.BallTree(m)
+new_dofs = zeros(length(m.dofs))
+energies = Float64[]
+for i = 0.1:0.1:10
+    for n in m.dofnodes
+        v = Vec((n.coord .+ Vec(i*1e-10,0.0,0.0))...)
+        d = BTO.Landau.extract_data(m, [v], m.dofs, tree)
+        new_dofs[n.dofs[:u]] .= d[1].u
+        new_dofs[n.dofs[:P]] .= d[1].P
+    end
+    push!(energies, F(new_dofs, m))
+end
+plot(energies)
+r
